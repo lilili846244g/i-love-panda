25cf8b8ee3c8134d941984a1164b2b10ed9ccae9
ly li  <lilili846244@gmail.com>
2023-10-23 17:14:49 +0800

    new

---
+++ assest/js/data.js
@@ -0,0 +1,94 @@
+const data = [
+    {
+        itemName:"name 1",
+        itemCategory:" 熊猫 1 ",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "1 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque  perferendis quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-1.jpeg",
+    },
+    {
+        itemName:"name 2",
+        itemCategory:" 熊猫 2 ",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "2 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id cupiditate aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-3.jpeg",
+    },
+    {
+        itemName:"name 3",
+        itemCategory:" 熊猫 3 ",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "3 Lorem, ipsum dolor sit amet  Quod atque id cupiditate exercitationem, perferendis quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-4.jpeg",
+    },
+    {
+        itemName:"name 4",
+        itemCategory:" 熊猫 4",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "4 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque idperferendis quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-5.jpeg",
+    },
+    {
+        itemName:"name 5",
+        itemCategory:" 熊猫 5 ",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "5 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id cupiditate exercitationem, perferendis iusto possimus.",
+        itemImg:"./assest/image/p-6.jpeg",
+    },
+
+    {
+        itemName:"name 6",
+        itemCategory:" 熊猫 6",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "7 Lorem, ipsum dolor sit amet consectetur adipisicing elit.  perferendis quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-7.jpeg",
+    },
+
+    {
+        itemName:"name 7",
+        itemCategory:" 熊猫 7 ",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "8 Lorem, ipsum dolor sit amet consectetur adipisicing elit.perferendis quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-8.jpeg",
+    },  
+    {
+        itemName:"name 9",
+        itemCategory:" 熊猫 9 ",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "9 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id cupiditate exercitationem, perferendis quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-10.jpeg",
+    },
+    {
+        itemName:"name 10",
+        itemCategory:" 熊猫 10",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "10 Lorem, ipsum dolor sit amet consectetur Quod atque id cupiditate exercitationem, perferendis quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-11.jpeg",
+    },
+    {
+        itemName:"name 11",
+        itemCategory:" 熊猫 11",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "11 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id  quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-12.jpg",
+    },
+    {
+        itemName:"name 12",
+        itemCategory:" 熊猫 12",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "11 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id  quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-12.jpg",
+    },
+];
+
+export { data };
