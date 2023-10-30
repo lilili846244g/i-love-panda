20c8452ebe0d4bf9ae6556523ae00a0d64fc66d0
ly li  <lilili846244@gmail.com>
2023-10-31 00:28:10 +0800

    new

--- assest/js/data.js
+++ assest/js/data.js
@@ -1,22 +1,22 @@
 const data = [
     {
-        itemName:"name 1",
-        itemCategory:" 熊猫 1 ",
-        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemName:"和花",
+        itemCategory:"雌性",
+        itemLink:"https://www.xiaohongshu.com/explore/6415920a000000002701077a",
         itemCopy:
-        "1 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque  perferendis quos quisquam aspernatur similique iusto possimus.",
-        itemImg:"./assest/image/p-1.jpeg",
+        "和花是一只成都大熊猫基地的大熊猫，于2020年7月4日与双胞胎弟弟和叶出生在成都大熊猫繁育研究基地月亮产房，初生体重200g，现居成都大熊猫繁育研究基地幼年园。母亲是大熊猫“成功”。 父亲是旅美大熊猫“美兰”,出生在美国亚特兰大动物园。",
+        itemImg:"./assest/image/花-1.png",
     },
     {
-        itemName:"name 2",
-        itemCategory:" 熊猫 2 ",
-        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemName:"福宝",
+        itemCategory:" 雌性 ",
+        itemLink:"https://www.xiaohongshu.com/explore/65338430000000001f03fe0f",
         itemCopy:
-        "2 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id cupiditate aspernatur similique iusto possimus.",
-        itemImg:"./assest/image/p-3.jpeg",
+        "福宝，雌性大熊猫，2020年7月20日出生于韩国三星爱宝乐园，母亲是大熊猫爱宝（国内呼名：华妮），父亲是大熊猫乐宝（国内呼名：园欣）。",
+        itemImg:"./assest/image/福-1.png",
     },
     {
-        itemName:"name 3",
+        itemName:"绩笑",
         itemCategory:" 熊猫 3 ",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
@@ -24,7 +24,7 @@ const data = [
         itemImg:"./assest/image/p-4.jpeg",
     },
     {
-        itemName:"name 4",
+        itemName:"二狗润玥",
         itemCategory:" 熊猫 4",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
@@ -32,7 +32,7 @@ const data = [
         itemImg:"./assest/image/p-5.jpeg",
     },
     {
-        itemName:"name 5",
+        itemName:"三太子萌兰",
         itemCategory:" 熊猫 5 ",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
@@ -41,7 +41,7 @@ const data = [
     },
 
     {
-        itemName:"name 6",
+        itemName:"天线宝宝奇一",
         itemCategory:" 熊猫 6",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
@@ -50,7 +50,7 @@ const data = [
     },
 
     {
-        itemName:"name 7",
+        itemName:"丫丫和乐乐",
         itemCategory:" 熊猫 7 ",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
@@ -58,23 +58,31 @@ const data = [
         itemImg:"./assest/image/p-8.jpeg",
     },  
     {
-        itemName:"name 9",
-        itemCategory:" 熊猫 9 ",
+        itemName:"福星胖大海",
+        itemCategory:" 熊猫 8 ",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
         "9 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id cupiditate exercitationem, perferendis quos quisquam aspernatur similique iusto possimus.",
         itemImg:"./assest/image/p-10.jpeg",
     },
     {
-        itemName:"name 10",
-        itemCategory:" 熊猫 10",
+        itemName:"圓潤女士",
+        itemCategory:" 熊猫 9",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
         "10 Lorem, ipsum dolor sit amet consectetur Quod atque id cupiditate exercitationem, perferendis quos quisquam aspernatur similique iusto possimus.",
         itemImg:"./assest/image/p-11.jpeg",
     },
     {
-        itemName:"name 11",
+        itemName:"和叶",
+        itemCategory:" 熊猫 10",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "11 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id  quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-12.jpg",
+    },
+    {
+        itemName:"梅兰",
         itemCategory:" 熊猫 11",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
@@ -82,12 +90,76 @@ const data = [
         itemImg:"./assest/image/p-12.jpg",
     },
     {
-        itemName:"name 12",
+        itemName:"福來",
         itemCategory:" 熊猫 12",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
         "11 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id  quos quisquam aspernatur similique iusto possimus.",
         itemImg:"./assest/image/p-12.jpg",
+    },
+    {
+        itemName:"蔓越煤",
+        itemCategory:" 熊猫 13",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "11 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id  quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-12.jpg",
+    },
+    {
+        itemName:"艾玖",
+        itemCategory:" 熊猫 14",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "11 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id  quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-12.jpg",
+    },
+    {
+        itemName:"成实",
+        itemCategory:" 熊猫 15",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "11 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id  quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-12.jpg",
+    },
+    {
+        itemName:"福气多",
+        itemCategory:" 熊猫 16",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "11 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id  quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-12.jpg",
+    },
+    {
+        itemName:"福气多",
+        itemCategory:" 熊猫 17",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "11 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id  quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-12.jpg",
+    },
+    {
+        itemName:"泰山",
+        itemCategory:" 熊猫 18",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "11 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id  quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-12.jpg",
+    },
+    {
+        itemName:"娇奥",
+        itemCategory:" 熊猫 19",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "11 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod atque id  quos quisquam aspernatur similique iusto possimus.",
+        itemImg:"./assest/image/p-12.jpg",
+    },
+    {
+        itemName:"七仔",
+        itemCategory:"雄性",
+        itemLink:"https://www.xiaohongshu.com/explore/64a3fed2000000002f02864d",
+        itemCopy:
+        "出生于2012年7月22日，2009年11月1日在陕西佛坪国家级自然保护区三官庙牌坊沟一带发现了一只棕色和白色相间的大熊猫，这只熊猫被命名为七仔。目前生活在陕西动物园研究中心。",
+        itemImg:"./assest/image/七-1.png",
     },
 ];
 
