70fa02945531eb80aecdd0d0fe06768e5da3cb3d
ly li  <lilili846244@gmail.com>
2023-11-18 23:39:30 +0800

    v

--- assest/js/data.js
+++ assest/js/data.js
@@ -46,8 +46,17 @@ const data = [
     },
     {
         itemBkg:"./assest/image/花-2.png",
+        itemName:"圓潤女士",
+        itemCategory:" 25.5萬粉",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "2012年8月25日出生在成都大熊猫繁育研究基地，初生体重137g。冰冰家族后代。父母是大熊猫园园和勇勇。",
+        itemImg:"./assest/image/p-11.jpeg",
+    },
+    {
+        itemBkg:"./assest/image/花-2.png",
         itemName:"福星胖大海",
-        itemCategory:" 熊猫 8 ",
+        itemCategory:" 14.8萬粉",
         itemLink:"https://www.xiaohongshu.com/explore/653e2a11000000001e00e4ae",
         itemCopy:
         "大熊猫福星于2017年6月25日出生于中国大熊猫保护研究中心雅安碧峰峡，谱系号1072，昵称胖大海，母亲是大熊猫瑛华，父亲是救助的野生大熊猫芦芦，在中心17届熊猫宝宝对外展出时，因体型巨大，毛色呈粉红色，在同一届熊猫宝宝中格外突出，被网友取名“胖大海”，之后，又因为一组蹲墙角的照片，爆红网络，成为新一代网红熊,目前居住在北京动物园",
@@ -56,13 +65,12 @@ const data = [
     {
         itemBkg:"./assest/image/丫丫-2.png",
         itemName:"丫丫和乐乐",
-        itemCategory:" 雌性 ",
+        itemCategory:" 14.1萬粉 ",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
         "丫丫雌性，谱系号507，2000年8月3日出生在北京动物园，母亲是大熊猫乐乐（谱系号320）。2003年4月，旅居美国田纳西州孟菲斯动物园 ，协议期限为10年，2013年到期后又延长了10年。2022年12月21日，孟菲斯动物园宣布，将把旅美大熊猫丫丫归还中国，结束20年的租借期。现居北京动物园",
         itemImg:"./assest/image/丫丫-1.png",
     }, 
-
     {
         itemBkg:"./assest/image/美香-1.png",
         itemName:"美香",
@@ -72,40 +80,19 @@ const data = [
         "1998年7月22日出生于中国大熊猫保护研究中心核桃坪野化培训基地，母亲是大熊猫雪雪。2000年12月6日，和大熊猫添添一起旅居在美国华盛顿国家动物园。当地时间2023年11月8日，旅美大熊猫“美香”“添添”和它们的幼崽“小奇迹”启程返回中国。  北京时间11月9日23时多，旅美大熊猫“美香”“添添”“小奇迹”抵达四川成都双流国际机场。",
         itemImg:"./assest/image/美香-2.png",
     },
-
-   
-   
-    {
-        itemBkg:"./assest/image/花-2.png",
-        itemName:"圓潤女士",
-        itemCategory:" 雌性",
-        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
-        itemCopy:
-        "2012年8月25日出生在成都大熊猫繁育研究基地，初生体重137g。冰冰家族后代。父母是大熊猫园园和勇勇。",
-        itemImg:"./assest/image/p-11.jpeg",
-    },
+  
     {
         itemBkg:"./assest/image/花-2.png",
         itemName:"和叶",
-        itemCategory:" 熊猫 10",
+        itemCategory:"13.6萬粉",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
         "和叶，雄性大熊猫，谱系号1238 ，于2020年7月4日与双胞胎姐姐和花 出生在成都大熊猫繁育研究基地月亮产房，初生体重162.6g，性格乖巧，有些敏感。母亲为“洁癖公主”大熊猫成功，出生于2000年9月11日 ，现居成都大熊猫繁育研究基地 。",
         itemImg:"./assest/image/p-12.jpg",
-    },
-    {
-        itemBkg:"./assest/image/花-2.png",
-        itemName:"白天",
-        itemCategory:" 雌性",
-        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
-        itemCopy:
-        "2018年8月20日出生在中国大熊猫保护研究中心碧峰峡基地，父母是大熊猫小白兔和芦芦。2020年10月16日，回到北京动物园。",
-        itemImg:"./assest/image/p-12.jpg",
-    },
-    {
+    },{
         itemBkg:"./assest/image/花-2.png",
         itemName:"梅兰",
-        itemCategory:" 雌性",
+        itemCategory:" 7.1萬粉",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
         "2016年5月29日出生在成都大熊猫繁育研究基地，父亲是大熊猫勇勇，母亲是海归大熊猫梅浜。肉肉的毛色比同龄小伙伴更黄，头大毛蓬。喜欢玩水，喜欢跟饲养员玩耍，特别爱撒娇。2016年12月28日 肉肉离开出生的太阳产房搬到月亮产房生活。",
@@ -113,8 +100,18 @@ const data = [
     },
     {
         itemBkg:"./assest/image/花-2.png",
+        itemName:"白天",
+        itemCategory:" 7萬粉",
+        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCopy:
+        "2018年8月20日出生在中国大熊猫保护研究中心碧峰峡基地，父母是大熊猫小白兔和芦芦。2020年10月16日，回到北京动物园。",
+        itemImg:"./assest/image/p-12.jpg",
+    },
+    
+    {
+        itemBkg:"./assest/image/花-2.png",
         itemName:"福來 ",
-        itemCategory:" 雌性",
+        itemCategory:" 6.3萬粉",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
         "2009年9月27日出生于中国大熊猫保护研究中心雅安碧峰峡基地，谱系号759。母亲帼帼，父亲大熊猫芦芦，兄弟姐妹是耀耀、亮亮、美安、大熊猫平平、安安、大熊猫雅吉、大熊猫花生和双胞胎妹妹大熊猫姚欣。",
@@ -123,7 +120,7 @@ const data = [
     {
         itemBkg:"./assest/image/花-2.png",
         itemName:"蔓越煤",
-        itemCategory:" 雌性",
+        itemCategory:" 6萬粉",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
         "2009年9月27日出生于中国大熊猫保护研究中心雅安碧峰峡基地，谱系号759。母亲帼帼，父亲大熊猫芦芦，兄弟姐妹是耀耀、亮亮、美安、大熊猫平平、安安、大熊猫雅吉、大熊猫花生和双胞胎妹妹大熊猫姚欣。",
@@ -132,7 +129,7 @@ const data = [
     {
         itemBkg:"./assest/image/花-2.png",
         itemName:"艾玖",
-        itemCategory:" 雌性",
+        itemCategory:" 5.2萬粉",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
         "2020年6月5日出生于成都大熊猫繁育基地，是艾莉麻麻的第三个孩子，出生时体重有219克，活妥妥一只小胖子，非常有活力，还吃了艾莉麻麻6克母乳，也是成都大熊猫繁育基地出生有记录以来最重的宝宝。",
@@ -150,37 +147,35 @@ const data = [
     {
         itemBkg:"./assest/image/花-2.png",
         itemName:"成实",
-        itemCategory:"雄性",
+        itemCategory:"4.9萬粉",
         itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
         itemCopy:
         "2016年8月19日，大熊猫“成绩”上午9点50分破羊水，下午13点产下一只雄性大熊猫幼仔。经验丰富的成绩妈妈见到孩子后，很快将其抱起进行舔舐。初生体重为205g的“成实”属“超重新生幼仔”。因为是“成绩”的第10个孩子，所以取名“成实”(谐音“十”)。 成实刚出生时和哥哥姐姐们都有一个共同特点——身上原本应该是黑色毛发的部分却是灰色的。灰胳膊、灰腿、灰眼圈、灰耳朵，看起来就是只灰白色的熊猫宝宝。",
         itemImg:"./assest/image/p-12.jpg",
     },
-   
-    
     {
-        itemBkg:"./assest/image/花-2.png",
+        itemBkg:"./assest/image/泰山-2.png",/* ok */
         itemName:"泰山",
-        itemCategory:"雄性",
-        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCategory:"3.5萬粉",
+        itemLink:"https://www.xiaohongshu.com/explore/64d8a027000000000c037f2e",
         itemCopy:
         "泰山，雄性大熊猫，谱系号595，2005年7月9日出生在美国华盛顿国家公园，父母是旅美大熊猫添添和美香；2010年2月5日，回到中国保护大熊猫研究中心雅安碧峰峡基地。泰山是我国第一部以大熊猫为主角的动画电影《我从中国来之熊猫泰山》的原型。",
-        itemImg:"./assest/image/p-12.jpg",
+        itemImg:"./assest/image/泰山-1.png",
     },
     {
-        itemBkg:"./assest/image/花-2.png",
+        itemBkg:"./assest/image/娇奥-1.png",/* ok */
         itemName:"娇奥",
-        itemCategory:"雌性",
-        itemLink:"https://www.xiaohongshu.com/explore/6525dfdc000000001c014bef",
+        itemCategory:"3.3萬粉",
+        itemLink:"https://www.xiaohongshu.com/explore/6465eda30000000027000ef8",
         itemCopy:
-        "享誉国际的英雄母亲——娇娇。娇娇是一只野生大熊猫，也是一位多产的妈妈，11年里生育了六个熊猫崽崽。娇娇成为了美国、英国、法国、日本、意大利等多国科学杂志的封面人物，也为现在长青保护区的建立做出了巨大贡献。",
-        itemImg:"./assest/image/p-12.jpg",
+        "出生于2016年8月15日四川成都大熊猫研究基地月亮产房。小时候毛特别炸特别好认。认猫插件是：腹黑，头上有呆毛，黑眼圈是16级最大的；肩带上缘像山峰；特别爱泡澡是个小澡星来着。爱耍赖，爱毁树，脸脸很奶，是个顶“尖儿”小帅哥，现在和双雄住在河南新乡五龙山野生动物王国。",
+        itemImg:"./assest/image/娇奥-2.png",
     },
     {
-        itemBkg:"./assest/image/花-2.png",
+        itemBkg:"./assest/image/七-2.png", /* ok */
         itemName:"七仔",
-        itemCategory:"雄性",
-        itemLink:"https://www.xiaohongshu.com/explore/64a3fed2000000002f02864d",
+        itemCategory:"3萬粉",
+        itemLink:"https://www.xiaohongshu.com/explore/64f7e7380000000020001a7a",
         itemCopy:
         "出生于2012年7月22日，2009年11月1日在陕西佛坪国家级自然保护区三官庙牌坊沟一带发现了一只棕色和白色相间的大熊猫，这只熊猫被命名为七仔。目前生活在陕西动物园研究中心。",
         itemImg:"./assest/image/七-1.png",
